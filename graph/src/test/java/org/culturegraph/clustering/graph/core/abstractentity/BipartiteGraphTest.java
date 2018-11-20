5cf967e0c014e9c21b8ac998903b0d62cdb0370f
eberhardtj <30794167+eberhardtj@users.noreply.github.com>
2018-11-20 14:46:53 +0100

    Add tests
    * bfs depth test for all graph variants
    * connected component iterator test for all graph variants

--- graph/src/test/java/org/culturegraph/clustering/graph/core/abstractentity/BipartiteGraphTest.java
+++ graph/src/test/java/org/culturegraph/clustering/graph/core/abstractentity/BipartiteGraphTest.java
@@ -1,18 +1,220 @@
 package org.culturegraph.clustering.graph.core.abstractentity;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Iterator;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import gnu.trove.set.TIntSet;
 import org.junit.Test;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 public class BipartiteGraphTest {
+
+    @Test
+    public void iterateConnectedComponentsForClique() throws Exception {
+        BipartiteGraph graph = loadGraph(clique);
+
+        Iterator<TIntSet> components = graph.iterator();
+
+        assertTrue(components.hasNext());
+
+        TIntSet component = components.next();
+        assertEquals(component.size(), 4);
+        assertTrue(component.contains(0));
+        assertTrue(component.contains(1));
+        assertTrue(component.contains(2));
+        assertTrue(component.contains(3));
+
+        assertFalse(components.hasNext());
+
+        Writer w = new BufferedWriter(new OutputStreamWriter(System.err));
+        graph.exportAsGraphML(w);
+        w.close();
+    }
+
+    @Test
+    public void iterateConnectedComponentsForLine() throws Exception {
+        BipartiteGraph graph = loadGraph(line);
+
+        Iterator<TIntSet> components = graph.iterator();
+
+        assertTrue(components.hasNext());
+
+        TIntSet component = components.next();
+        assertEquals(component.size(), 4);
+        assertTrue(component.contains(0));
+        assertTrue(component.contains(1));
+        assertTrue(component.contains(2));
+        assertTrue(component.contains(3));
+
+        assertFalse(components.hasNext());
+
+        Writer w = new BufferedWriter(new OutputStreamWriter(System.err));
+        graph.exportAsGraphML(w);
+        w.close();
+    }
+
+    @Test
+    public void iterateConnectedComponentsForRing() throws Exception {
+        BipartiteGraph graph = loadGraph(ring);
+
+        Iterator<TIntSet> components = graph.iterator();
+
+        assertTrue(components.hasNext());
+
+        TIntSet component = components.next();
+        assertEquals(component.size(), 4);
+        assertTrue(component.contains(0));
+        assertTrue(component.contains(1));
+        assertTrue(component.contains(2));
+        assertTrue(component.contains(3));
+
+        assertFalse(components.hasNext());
+
+        Writer w = new BufferedWriter(new OutputStreamWriter(System.err));
+        graph.exportAsGraphML(w);
+        w.close();
+    }
+
+    @Test
+    public void iterateConnectedComponentsForStar() throws Exception {
+        BipartiteGraph graph = loadGraph(star);
+
+        Iterator<TIntSet> components = graph.iterator();
+
+        assertTrue(components.hasNext());
+
+        TIntSet component = components.next();
+        assertEquals(component.size(), 4);
+        assertTrue(component.contains(0));
+        assertTrue(component.contains(1));
+        assertTrue(component.contains(2));
+        assertTrue(component.contains(3));
+
+        assertFalse(components.hasNext());
+
+        Writer w = new BufferedWriter(new OutputStreamWriter(System.err));
+        graph.exportAsGraphML(w);
+        w.close();
+    }
+
+    @Test
+    public void iterateConnectedComponentsForRandom() throws Exception {
+        BipartiteGraph graph = loadGraph(simple);
+
+        Iterator<TIntSet> components = graph.iterator();
+
+        assertTrue(components.hasNext());
+        TIntSet component1 = components.next();
+        assertEquals(component1.size(), 3);
+        assertTrue(component1.contains(0));
+        assertTrue(component1.contains(2));
+        assertTrue(component1.contains(3));
+
+        assertTrue(components.hasNext());
+        TIntSet component2 = components.next();
+        assertEquals(component2.size(), 1);
+        assertTrue(component2.contains(1));
+
+        assertFalse(components.hasNext());
+
+        Writer w = new BufferedWriter(new OutputStreamWriter(System.err));
+        graph.exportAsGraphML(w);
+        w.close();
+    }
+
+    @Test
+    public void stopBfsAfterDepthOfZero() throws Exception {
+        BipartiteGraph graph = loadGraph(line);
+
+        int startRow = 0;
+
+        TIntSet bfs = graph.breadthFirstSearch(startRow, 0);
+
+        List<Integer> actual = Arrays.stream(bfs.toArray())
+                .boxed()
+                .collect(Collectors.toList());
+
+        assertEquals(actual.size(), 1);
+        assertTrue(actual.contains(0));
+    }
+
+    @Test
+    public void stopBfsAfterDepthOfOne() throws Exception {
+        BipartiteGraph graph = loadGraph(line);
+
+        int startRow = 0;
+
+        TIntSet bfs = graph.breadthFirstSearch(startRow, 1);
+
+        List<Integer> actual = Arrays.stream(bfs.toArray())
+                .boxed()
+                .collect(Collectors.toList());
+
+        assertEquals(actual.size(), 2);
+        assertTrue(actual.contains(0));
+        assertTrue(actual.contains(1));
+    }
+
+    @Test
+    public void stopBfsAfterDepthOfTwo() throws Exception {
+        BipartiteGraph graph = loadGraph(line);
+
+        int startRow = 0;
+
+        TIntSet bfs = graph.breadthFirstSearch(startRow, 2);
+
+        List<Integer> actual = Arrays.stream(bfs.toArray())
+                .boxed()
+                .collect(Collectors.toList());
+
+        assertEquals(actual.size(), 3);
+        assertTrue(actual.contains(0));
+        assertTrue(actual.contains(1));
+        assertTrue(actual.contains(2));
+    }
+
+    @Test
+    public void stopBfsAfterDepthOfThree() throws Exception {
+        BipartiteGraph graph = loadGraph(line);
+
+        int startRow = 0;
+
+        TIntSet bfs = graph.breadthFirstSearch(startRow, 3);
+
+        List<Integer> actual = Arrays.stream(bfs.toArray())
+                .boxed()
+                .collect(Collectors.toList());
+
+        assertEquals(actual.size(), 4);
+        assertTrue(actual.contains(0));
+        assertTrue(actual.contains(1));
+        assertTrue(actual.contains(2));
+        assertTrue(actual.contains(3));
+    }
+
+    @Test
+    public void bfsShouldReturnComponentIfDepthIsLargerThanGraphDepth() throws Exception {
+        BipartiteGraph graph = loadGraph(line);
+
+        int startRow = 0;
+
+        TIntSet bfs = graph.breadthFirstSearch(startRow, 10);
+
+        List<Integer> actual = Arrays.stream(bfs.toArray())
+                .boxed()
+                .collect(Collectors.toList());
+
+        assertEquals(actual.size(), 4);
+        assertTrue(actual.contains(0));
+        assertTrue(actual.contains(1));
+        assertTrue(actual.contains(2));
+        assertTrue(actual.contains(3));
+    }
+
     @Test
     public void fromAdjacencyList() throws Exception
     {
@@ -43,7 +245,7 @@ public class BipartiteGraphTest {
         BipartiteGraph graph = loadGraph(simple);
 
         boolean[] actual = new boolean[4];
-        for (int elem: graph.breathFirstSearch(0).toArray())
+        for (int elem: graph.breadthFirstSearch(0).toArray())
         {
             actual[elem] = true;
         }
@@ -58,7 +260,7 @@ public class BipartiteGraphTest {
         BipartiteGraph graph = loadGraph(simple);
 
         boolean[] actual = new boolean[4];
-        for (int elem: graph.breathFirstSearch(2).toArray())
+        for (int elem: graph.breadthFirstSearch(2).toArray())
         {
             actual[elem] = true;
         }
@@ -73,7 +275,7 @@ public class BipartiteGraphTest {
         BipartiteGraph graph = loadGraph(simple);
 
         boolean[] actual = new boolean[4];
-        for (int elem: graph.breathFirstSearch(3).toArray())
+        for (int elem: graph.breadthFirstSearch(3).toArray())
         {
             actual[elem] = true;
         }
@@ -89,7 +291,7 @@ public class BipartiteGraphTest {
 
         for (int i = 0; i < graph.getRows(); i++)
         {
-            graph.breathFirstSearch(i);
+            graph.breadthFirstSearch(i);
         }
     }
 
@@ -100,7 +302,7 @@ public class BipartiteGraphTest {
 
         for (int i = 0; i < graph.getRows(); i++)
         {
-            graph.breathFirstSearch(i);
+            graph.breadthFirstSearch(i);
         }
     }
 
@@ -111,7 +313,7 @@ public class BipartiteGraphTest {
 
         for (int i = 0; i < graph.getRows(); i++)
         {
-            graph.breathFirstSearch(i);
+            graph.breadthFirstSearch(i);
         }
     }
 
@@ -148,9 +350,10 @@ public class BipartiteGraphTest {
         }
     }
 
-    final private Path data = Paths.get("src\\test\\resources");
+    final private Path data = Paths.get("src/test/resources");
     private Path clique = data.resolve("graphs/clique.txt");
     private Path ring = data.resolve("graphs/ring.txt");
     private Path star = data.resolve("graphs/star.txt");
     private Path simple = data.resolve("graphs/random.txt");
+    private Path line = data.resolve("graphs/line.txt");
 }
