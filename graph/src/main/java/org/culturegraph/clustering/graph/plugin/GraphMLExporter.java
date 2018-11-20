e52c6cfb68af42cf5d6d7d979c979645d77b09bf
eberhardtj <30794167+eberhardtj@users.noreply.github.com>
2018-11-20 14:36:41 +0100

    Add GraphML exporter

---
+++ graph/src/main/java/org/culturegraph/clustering/graph/plugin/GraphMLExporter.java
@@ -0,0 +1,98 @@
+package org.culturegraph.clustering.graph.plugin;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public class GraphMLExporter {
+
+    private Writer write;
+    private boolean lock;
+
+    public GraphMLExporter(Writer  writer) {
+        this.write = writer;
+        this.lock = false;
+    }
+
+    private String declaration() {
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+    }
+
+    private String openGraphml() {
+        return "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\"  \n" +
+                "      xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \n" +
+                "      xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns \n" +
+                "        http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\">";
+    }
+
+    private String closeGraphml() {
+        return "</graphml>";
+    }
+
+    private String defineKeyLabel() {
+        return "<key id=\"d0\" for=\"node\" attr.name=\"label\" attr.type=\"string\"/>";
+    }
+
+    private String defineKeyComponent() {
+        return "<key id=\"d1\" for=\"all\" attr.name=\"component\" attr.type=\"int\"/>";
+    }
+
+    private String openGraph() {
+        return "<graph id=\"G\" edgedefault=\"undirected\" parse.order=\"nodesfirst\">";
+    }
+
+    private String closeGraph() {
+        return "</graph>";
+    }
+
+    private void stop() {
+        throw new IllegalArgumentException("Can't insert a node after a edge in a nodesfirst graphml document.");
+    }
+
+    public void push(String s) throws IOException {
+        write.write(s + "\n");
+    }
+
+    public void start() throws IOException {
+        push(declaration());
+        push(openGraphml());
+        push(defineKeyLabel());
+        push(defineKeyComponent());
+        push(openGraph());
+    }
+
+    public void addNode(String nodeId) throws IOException {
+        if (lock) stop();
+        String node = "<node id=\"" + nodeId + "\"/>";
+        push(node);
+    }
+
+    public void addNode(String nodeId, String label) throws IOException {
+        if (lock) stop();
+        String node = "<node id=\"" + nodeId + "\"><data key=\"d0\">" + label + "</data></node>";
+        push(node);
+    }
+
+    public void addNode(String nodeId, String label, int component) throws IOException {
+        if (lock) stop();
+        String node = "<node id=\"" + nodeId + "\"><data key=\"d0\">" + label + "</data><data key=\"d1\">" + component + "</data></node>";
+        push(node);
+    }
+
+    public void addEdge(int id, String sourceId, String targetId) throws IOException {
+        if (!lock) lock = true;
+        String edge = "<edge id=\"e" + id + "\" source=\"" + sourceId + "\" target=\"" +  targetId + "\"></edge>";
+        push(edge);
+    }
+
+    public void addEdge(int id, String sourceId, String targetId, int component) throws IOException {
+        if (!lock) lock = true;
+        String edge = "<edge id=\"e" + id + "\" source=\"" + sourceId + "\" target=\"" +  targetId + "\"><data key=\"d1\">" + component + "</data></edge>";
+        push(edge);
+    }
+
+    public void end() throws IOException {
+        push(closeGraph());
+        push(closeGraphml());
+    }
+
+}
---
+++ graph/src/test/java/org/culturegraph/clustering/graph/plugin/GraphMLExporterTest.java
@@ -0,0 +1,58 @@
+package org.culturegraph.clustering.graph.plugin;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.*;
+
+public class GraphMLExporterTest {
+
+    private Charset utf8 = StandardCharsets.UTF_8;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void createGraphML() throws Exception {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        try (OutputStreamWriter writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
+             BufferedWriter br = new BufferedWriter(writer)) {
+
+            GraphMLExporter exporter = new GraphMLExporter(br);
+            exporter.start();
+            exporter.addNode("n" + 1, "a", 1);
+            exporter.addNode("n" + 2, "b", 1);
+            exporter.addNode("n" + 3, "c", 2);
+            exporter.addEdge(1, "n" + 1, "n" + 2, 1);
+            exporter.end();
+        }
+        outputStream.close();
+
+        String actual = outputStream.toString(utf8.name());
+
+        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\"  \n" +
+                "      xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+                "      xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns \n" +
+                "        http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\">\n" +
+                "<key id=\"d0\" for=\"node\" attr.name=\"label\" attr.type=\"string\"/>\n" +
+                "<key id=\"d1\" for=\"all\" attr.name=\"component\" attr.type=\"int\"/>\n" +
+                "<graph id=\"G\" edgedefault=\"undirected\" parse.order=\"nodesfirst\">\n" +
+                "<node id=\"n1\"><data key=\"d0\">a</data><data key=\"d1\">1</data></node>\n" +
+                "<node id=\"n2\"><data key=\"d0\">b</data><data key=\"d1\">1</data></node>\n" +
+                "<node id=\"n3\"><data key=\"d0\">c</data><data key=\"d1\">2</data></node>\n" +
+                "<edge id=\"e1\" source=\"n1\" target=\"n2\"><data key=\"d1\">1</data></edge>\n" +
+                "</graph>\n" +
+                "</graphml>\n";
+
+        assertEquals(actual, expected);
+    }
+}
